50d5c2efe40bb78bad1ae1f4a0f042da177b4980
khownz <koen895@hotmail.com>
2023-08-03 21:47:34 +0200

    added basic search bar & filtering mechanism

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,30 @@ import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {COLORS} from './data/colors';
 import {ColorTileComponent} from './components/color-tile/color-tile.component';
+import {SearchBarComponent} from './components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ColorTileComponent],
+  imports: [CommonModule, RouterOutlet, ColorTileComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'color-picker';
-  colors = COLORS;
+  filteredColors = COLORS;
+
+  filter(searchTerm: string): void {
+    // TODO: more lenient search mechanism (fuzzy search)
+    // TODO: double check if min 3 chars before search is desired behaviour
+    if (searchTerm.length >= 3) {
+      this.filteredColors = COLORS.filter((color) => {
+        const nameMatches = color.name.toLowerCase().includes(searchTerm.toLowerCase());
+        const hexMatches = color.hex.toLowerCase().includes(searchTerm.toLowerCase());
+        return nameMatches || hexMatches;
+      });
+    } else {
+      this.filteredColors = COLORS;
+    }
+  }
 }
