ffedcae16b01f33dc7ec04b7798a82683fc01570
khownz <koen895@hotmail.com>
2023-10-05 20:48:06 +0200

    Extracted colors + added copy icon on tile hover

--- src/app/components/color-tile/color-tile.component.ts
+++ src/app/components/color-tile/color-tile.component.ts
@@ -1,11 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Color} from '../../types/color';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-color-tile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatIconModule, MatTooltipModule],
   templateUrl: './color-tile.component.html',
   styleUrls: ['./color-tile.component.scss'],
 })
